2b531701d22a022b11521efc2cb956e69dd35c34
Darudatwo# <daniel.hodgett0904@gmail.com>
2022-12-11 16:45:33 +0000

    improved account creation functionality

--- src/JS/app.js
+++ src/JS/app.js
@@ -8,7 +8,6 @@ CreateUserURL = "https://prod-28.ukwest.logic.azure.com/workflows/b12c3e6b6ccc49
 UserCheckStart="https://prod-03.uksouth.logic.azure.com/workflows/fbed9434db034c459d4812d31d677199/triggers/manual/paths/invoke/rest/v1/assests/?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=WOajqr-nTCusNB5iSre90uOd5MXFJA3kYg-7fRNRvoM";
 UserCheckEnd = "?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=WOajqr-nTCusNB5iSre90uOd5MXFJA3kYg-7fRNRvoM";
 CheckUserExitsURL = "https://prod-15.centralus.logic.azure.com:443/workflows/b3a4e28aa0d34b7e8a5c63279fefded7/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=idqwk0_RtWX2UlcBJhGU-zK434GYL9KbG09fiHjpAFQ";
-CheckUsernameIsFree = "https://prod-17.centralus.logic.azure.com:443/workflows/68cfac6afa0d4ecaa168fde59ffb4628/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ABbY8hDqsf2cS5Kq2jXliAX5ubiENoRlq77VCPD9kL0";
 ChangePasswordURL_start = "https://prod-17.centralus.logic.azure.com/workflows/6d5892483b5c43249fe6f15427d0eeb4/triggers/manual/paths/invoke/rest/v1/assets/";
 ChangePasswordUrl_end = "?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=YLqf8QLCya8Q7skf8n14noi008XF4hrMQVGuKwxF-2A";
 
@@ -170,48 +169,35 @@ function createUser(){
 
   if(password==passwordConfirm){
     
-  checkUsernameData = new FormData();
-  checkUsernameData.append('userName', username)
+    createUserData = new FormData();
+    createUserData.append('userName', username)
+    createUserData.append('userFullName', fullname)
+    createUserData.append('userPassword', password)
 
     $.ajax({
-      url: CheckUsernameIsFree,
+      url: CreateUserURL,
       type:"POST",
-      data: checkUsernameData,
+      data: createUserData,
       cache: false,
       contentType: false,
       processData: false,
       success: function(response){
         console.log(response)
-        if(!response){
-          console.log("works")
-
-          createUserData = new FormData();
-          createUserData.append('userName', username)
-          createUserData.append('userFullName', fullname)
-          createUserData.append('userPassword', password)
-
-          $.ajax({
-            url: CreateUserURL,
-            type:"POST",
-            data: createUserData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(response){
-              console.log(response)
-
-            }
-          })
-        } else{
+        if(response == true){
           alert("Username Taken");
+        } else{
+          alert("Account Created")
+          sessionStorage.setItem("userId", response.userId)
+          sessionStorage.setItem("userName", response.userName)
+          sessionStorage.setItem("isAdmin", response.isAdmin)
+          sessionStorage.setItem("userFullName", response.userFullName)
+          window.location = "./home.html";
         }
       }
     })
   } else{
     alert("Passwords do not match")
   }
-
-
 }
 
 
